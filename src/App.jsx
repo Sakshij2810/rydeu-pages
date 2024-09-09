3debf851ab116bde874a3144ed406dc2532c09f3
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-09-09 11:59:26 +0530

    cahnges in routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import CityPage from "./pages/CityPage/CityPage";
 import BookingRequest from "./pages/BookingRequest/BookingRequest";
+import HomePage from "./pages/HomePage/HomePage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/city" element={<CityPage />} />
+        <Route exact path="/home-page" element={<HomePage />} />
+        <Route exact path="/city-page" element={<CityPage />} />
         <Route exact path="/booking-request" element={<BookingRequest />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page-main-container">
+      <ol>
+        <li>
+          {" "}
+          <Link to="/home-page">Home Page</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/city-page">City Page</Link>
+        </li>
+        <li>
+          <Link to="/booking-request">Booking Request Page</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 export default Home;
