2deb880a451a210f146f8890a7000f7df937f1a0
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-09-09 15:11:40 +0530

    home-page-added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,17 @@
+import Header from "../../components/HomePageComponents/Header/Header";
+import "./HomePage.css";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="home-page-content-container">
+      <div className="home-page-all-conetnt-div">
+        <div className="header-container">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
